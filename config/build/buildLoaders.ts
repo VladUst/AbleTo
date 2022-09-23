0a3145dbccfbf9e791416c47f5f0a6e964161a30
VladUs <vladustyugo@gmail.com>
2022-09-23 23:51:39 +0700

    Button ui and svg/png/jpg/jpeg support

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,36 +3,53 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): RuleSetRule[]{
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
     const cssLoader = {
-            test: /\.s[ac]ss$/i,
-            use: [
-                // Creates `style` nodes from JS strings
-                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                // Translates CSS into CommonJS
-                {
-                    loader: "css-loader",
-                    options: {
-                        modules: {
-                            //вернет true если module и обработает как модули
-                            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                            localIdentName: isDev ?
-                                '[path][name]__[local]--[hash:base64:5]'
-                                : '[hash:base64:8]'
-                        },
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        //вернет true если module и обработает как модули
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ?
+                            '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
                     },
                 },
-                // Compiles Sass to CSS
-                "sass-loader",
-            ],
-        };
+            },
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    };
     //без ts понадобился бы транспилятор babel
     const typescriptLoader = {
         test: /\.tsx?$/, // переводить tsx в ts
         use: 'ts-loader',
         exclude: /node_modules/,
     }
+
     return [
         typescriptLoader,
         cssLoader,
+        svgLoader,
+        fileLoader
     ]
 }
--- src/app/providers/ThemeProvider/index.tsx
+++ src/app/providers/ThemeProvider/index.tsx
@@ -1,4 +1,3 @@
-import ThemeProvider  from './ui/ThemeProvider';
-import {useTheme} from "./lib/useTheme";
-
-export {ThemeProvider, useTheme};
+export {default as ThemeProvider}  from './ui/ThemeProvider';
+export {useTheme} from "./lib/useTheme";
+export {Theme} from './lib/ThemeContext';
