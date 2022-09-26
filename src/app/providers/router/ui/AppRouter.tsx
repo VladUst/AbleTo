14fb0522f76a6f482927732dd1fb071b81dfe258
VladUs <vladustyugo@gmail.com>
2022-09-26 16:10:23 +0700

    unresolved root and loaders

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,20 +1,21 @@
 import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader'
 
 const AppRouter = () => {
   return (
-      <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-              {Object.values(routeConfig).map(route =>
-                  <Route key={route.path} path={route.path} element={(
+      <Routes>
+          {Object.values(routeConfig).map(route =>
+              <Route key={route.path} path={route.path} element={(
+                  <Suspense fallback={<PageLoader/>}>
                       <div className="page-wrapper">
                           {route.element}
                       </div>
+                  </Suspense>
                     )}/>
-              )}
-          </Routes>
-      </Suspense>
+          )}
+      </Routes>
   )
 }
 
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import { useTranslation } from 'react-i18next'
 
 interface NavbarProps {
   className?: string
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation()
   return (
       <div className={classNames(cls.Navbar, {}, [className])}>
           <div className={cls.links}>
               <AppLink to='/'
                        className={cls.mainLink}
-                       theme={AppLinkTheme.SECONDARY}>Главная</AppLink>
-              <AppLink to='/about' theme={AppLinkTheme.SECONDARY}>О сайте</AppLink>
+                       theme={AppLinkTheme.SECONDARY}>{t('Главная')}</AppLink>
+              <AppLink to='/about' theme={AppLinkTheme.SECONDARY}>{t('О сайте')}</AppLink>
           </div>
       </div>
   )
