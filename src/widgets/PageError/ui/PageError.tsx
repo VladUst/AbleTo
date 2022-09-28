c7f5ce644074c645ec8870c21781a15c8ec734d2
VladUs <vladustyugo@gmail.com>
2022-09-28 18:54:37 +0700

    error boundary wrapper created

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,21 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './PageError.module.scss'
+import { useTranslation } from 'react-i18next'
+interface PageErrorProps {
+  className?: string
+}
+
+export const PageError = ({ className }: PageErrorProps) => {
+  const { t } = useTranslation()
+
+  const reload = () => {
+    location.reload()
+  }
+
+  return (
+      <div className={classNames(cls.PageError, {}, [className])}>
+          <p>{t('Произошла непредвиденная ошибка')}</p>
+          <button onClick={reload}>{t('Перезагрузить страницу')}</button>
+      </div>
+  )
+}
