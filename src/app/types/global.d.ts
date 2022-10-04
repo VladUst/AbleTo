8b4c453a1caf2ac039da603051ff707e437f0661
VladUs <vladustyugo@gmail.com>
2022-10-04 19:02:06 +0700

    base CI actions commands

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -10,8 +10,8 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg' {
-  const content: any
-  export default content
+  import React from 'react'
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+  export default SVG
 }
-
 declare const _IS_DEV_: boolean
