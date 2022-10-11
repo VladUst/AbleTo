2e8e42b49bdbfb952d952dfa8e20cb4e0b5b311c
VladUs <vladustyugo@gmail.com>
2022-10-11 22:31:24 +0700

    sidebar refactoring + new tests and loki reg-cli

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import { Button, ThemeButton } from './Button'
+import { Button, ButtonSize, ButtonTheme } from './Button'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator'
 import { Theme } from 'app/providers/ThemeProvider'
 
@@ -22,18 +22,79 @@ Primary.args = {
 export const Clear = Template.bind({})
 Clear.args = {
   children: 'Text',
-  theme: ThemeButton.CLEAR
+  theme: ButtonTheme.CLEAR
 }
 
 export const Outline = Template.bind({})
 Outline.args = {
   children: 'Text',
-  theme: ThemeButton.OUTLINE
+  theme: ButtonTheme.OUTLINE
 }
 
 export const OutlineDark = Template.bind({})
 OutlineDark.args = {
   children: 'Text',
-  theme: ThemeButton.OUTLINE
+  theme: ButtonTheme.OUTLINE
 }
 OutlineDark.decorators = [ThemeDecorator(Theme.DARK)]
+
+export const OutlineL = Template.bind({})
+OutlineL.args = {
+  children: 'Text',
+  theme: ButtonTheme.OUTLINE,
+  size: ButtonSize.L
+}
+export const OutlineM = Template.bind({})
+OutlineM.args = {
+  children: 'Text',
+  theme: ButtonTheme.OUTLINE,
+  size: ButtonSize.M
+}
+export const OutlineXL = Template.bind({})
+OutlineXL.args = {
+  children: 'Text',
+  theme: ButtonTheme.OUTLINE,
+  size: ButtonSize.XL
+}
+
+export const BackgroundTheme = Template.bind({})
+BackgroundTheme.args = {
+  children: 'Text',
+  theme: ButtonTheme.BACKGROUND
+}
+
+export const BackgroundInvertedTheme = Template.bind({})
+BackgroundInvertedTheme.args = {
+  children: 'Text',
+  theme: ButtonTheme.BACKGROUND_INVERTED
+}
+
+export const Square = Template.bind({})
+Square.args = {
+  children: '>',
+  theme: ButtonTheme.BACKGROUND_INVERTED,
+  square: true
+}
+export const SquareL = Template.bind({})
+SquareL.args = {
+  children: '>',
+  theme: ButtonTheme.BACKGROUND_INVERTED,
+  square: true,
+  size: ButtonSize.L
+}
+
+export const SquareM = Template.bind({})
+SquareM.args = {
+  children: '>',
+  theme: ButtonTheme.BACKGROUND_INVERTED,
+  square: true,
+  size: ButtonSize.M
+}
+
+export const SquareXL = Template.bind({})
+SquareXL.args = {
+  children: '>',
+  theme: ButtonTheme.BACKGROUND_INVERTED,
+  square: true,
+  size: ButtonSize.XL
+}
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,12 +3,20 @@ import cls from './Sidebar.module.scss'
 import React, { useState } from 'react'
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher'
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import { useTranslation } from 'react-i18next'
+import { RoutePath } from 'shared/config/routeConfig/routeConfig'
+import AboutIcon from 'shared/assets/icons/about.svg'
+import MainIcon from 'shared/assets/icons/main.svg'
+
 interface SidebarProps {
   className?: string
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false)
+  const { t } = useTranslation()
 
   const onToggle = () => {
     setCollapsed(prev => !prev)
@@ -17,11 +25,32 @@ export const Sidebar = ({ className }: SidebarProps) => {
   return (
       <div data-testid="sidebar"
           className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-          <button data-testid="sidebar-toggle"
-              onClick={onToggle}>toggle</button>
+          <Button data-testid="sidebar-toggle"
+              onClick={onToggle}
+              className={cls.collapseBtn}
+              theme={ButtonTheme.BACKGROUND_INVERTED}
+              size={ButtonSize.XL}
+              square
+          >
+              {collapsed ? '>' : '<'}
+          </Button>
+          <div className={cls.items}>
+              <AppLink to={RoutePath.main}
+                           className={cls.item}
+                           theme={AppLinkTheme.SECONDARY}>
+                  <MainIcon className={cls.icon}/>
+                  <span className={cls.link}>{t('Главная')}</span>
+              </AppLink>
+              <AppLink to={RoutePath.about}
+                       className={cls.item}
+                       theme={AppLinkTheme.SECONDARY}>
+                  <AboutIcon className={cls.icon}/>
+                  <span className={cls.link}>{t('О сайте')}</span>
+              </AppLink>
+          </div>
           <div className={cls.switchers}>
               <ThemeSwitcher/>
-              <LangSwitcher className={cls.lang}/>
+              <LangSwitcher short={collapsed} className={cls.lang}/>
           </div>
       </div>
   )
