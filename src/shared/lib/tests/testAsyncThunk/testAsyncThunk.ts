06ff3927a624f49aa7743dd5a4b4b3db5af56456
VladUs <vladustyugo@gmail.com>
2023-01-21 20:15:46 +0700

    WIP try to fix testAsyncThunk.ts

--- src/shared/lib/tests/testAsyncThunk/testAsyncThunk.ts
+++ src/shared/lib/tests/testAsyncThunk/testAsyncThunk.ts
@@ -15,21 +15,30 @@ export class TestAsyncThunk<Return, Arg, RejectedValue> {
   getState: () => StateSchema;
 
   actionCreator: ActionCreatorType<Return, Arg, RejectedValue>;
+
   api: jest.MockedFunctionDeep<AxiosStatic>;
+
   navigate: jest.MockedFn<any>;
 
-  constructor (actionCreator: ActionCreatorType<Return, Arg, RejectedValue>,
-    state?: DeepPartial<StateSchema>) {
+  constructor (
+    actionCreator: ActionCreatorType<Return, Arg, RejectedValue>,
+    state?: DeepPartial<StateSchema>
+  ) {
     this.actionCreator = actionCreator;
     this.dispatch = jest.fn();
     this.getState = jest.fn(() => state as StateSchema);
+
     this.api = mockedAxios;
     this.navigate = jest.fn();
   }
 
   async callThunk (arg: Arg) {
     const action = this.actionCreator(arg);
-    const result = await action(this.dispatch, this.getState, { api: this.api, navigate: this.navigate });
+    const result = await action(
+      this.dispatch,
+      this.getState,
+      { api: this.api, navigate: this.navigate }
+    );
 
     return result;
   }
