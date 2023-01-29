cee50ba0dc826ca06e27754ddd8f2e5fb1b1b2f3
VladUs <vladustyugo@gmail.com>
2023-01-29 15:16:39 +0700

    article views switcher

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
@@ -1,6 +1,3 @@
 import React from 'react';
 
-export const ArticlesPageAsync = React.lazy(async () => await new Promise(resolve => {
-  // @ts-expect-error
-  setTimeout(() => resolve(import('./ArticlesPage')), 1500);
-}));
+export const ArticlesPageAsync = React.lazy(async () => await import('./ArticlesPage'));
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,16 +1,49 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './ArticlesPage.module.scss';
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader';
+import { articlesPageActions, articlesPageReducer, getArticles } from '../../model/slices/articlesPageSlice';
+import { ArticleList, ArticleView, ArticleViewSelector } from 'entities/Article';
+import { useDispatch, useSelector } from 'react-redux';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
+import {
+  getArticlesPageIsLoading,
+  getArticlesPageView
+} from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
 
 interface ArticlesPageProps {
   className?: string
 }
 
+const reducers: ReducersList = {
+  articlesPage: articlesPageReducer
+};
+
 const ArticlesPage = ({ className }: ArticlesPageProps) => {
+  const dispatch = useDispatch();
+  const articles = useSelector(getArticles.selectAll);
+  const isLoading = useSelector(getArticlesPageIsLoading);
+  const view = useSelector(getArticlesPageView);
+  const onChangeView = useCallback((view: ArticleView) => {
+    dispatch(articlesPageActions.setView(view));
+  }, [dispatch]);
+  useInitialEffect(() => {
+    dispatch(fetchArticlesList());
+    dispatch(articlesPageActions.initState());
+  });
   return (
-      <div className={classNames(cls.ArticlesPage, {}, [className])}>
-          articles
-      </div>
+      <DynamicModuleLoader reducers={reducers}>
+          <div className={classNames(cls.ArticlesPage, {}, [className])}>
+              <ArticleViewSelector view={view} onViewClick={onChangeView}/>
+              <ArticleList
+                  isLoading={isLoading}
+                  view={view}
+                  articles={articles}
+              />
+          </div>
+      </DynamicModuleLoader>
+
   );
 };
 
